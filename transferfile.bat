785769e5af92533585b9e045aaad8073be478fcd
Shubham <mauryask1732@gmail.com>
2025-05-21 00:38:49 +0530

    Delete transferfile.bat

--- transferfile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p SRCURL="Enter source URL: "
-set /p DESTURL="Enter dstination URL: "
-curl %SRCURL% -o %DESTURL%
-::curl -d @%SRCURL% -u sroot %DESTURL%
-echo "File transfered successfully.." 
